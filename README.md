ae0641370051be4b0a86d935a9a69db399b0c1a5
Aleks Hudochenkov <aleks@hudochenkov.com>
2020-02-25 01:33:14 +0100

    Initial config

---
+++ README.md
@@ -0,0 +1,19 @@
+# prettier-config-hudochenkov
+
+My config for [Prettier](https://prettier.io/).
+
+## Usage
+
+Install:
+
+```
+npm install --save-dev prettier-config-hudochenkov
+```
+
+Add the following in `package.json`:
+
+```
+"prettier": "prettier-config-hudochenkov"
+```
+
+Refer to [Prettier documentation](https://prettier.io/docs/en/configuration.html#sharing-configurations), if you need override some options.
---
+++ index.js
@@ -0,0 +1,23 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	printWidth: 100,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['*.css', '*.pcss', '*.scss'],
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
+};
